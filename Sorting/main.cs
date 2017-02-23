c159d3f265bde6724a73ffe85a8c4f64a3ee9ad6
chrisleedeveloper34 <chrisleedeveloper34@users.noreply.github.com>
2017-02-23 15:36:13 +1100

    Created by Coding Ground Client

---
+++ Sorting/main.cs
@@ -0,0 +1,143 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Swap(int[] data, int pos1, int pos2)
+    {
+        Console.WriteLine($"Swapping pos{pos1} value {data[pos1]} and pos{pos2} value {data[pos2]}");
+        int temp = data[pos1];
+        data[pos1] = data[pos2];
+        data[pos2] = temp;
+    }
+    
+    static void BubbleSort(int[] data)
+    {
+        for(int j = data.Length-1; j > 0; j--)
+        {
+            for(int i = 0; i < j; i++)
+            {
+                //Console.WriteLine($"i = {i}, j = {j}");
+                if (data[i] > data[i+1])
+                {
+                    Swap(data, i, i+1);
+                    string numbers = String.Join(",", data);
+                    Console.WriteLine(numbers);
+                }
+            }
+        }
+    }
+    
+    static int IntMinArray(int[] data, int start)
+    {
+        int min = start;
+        for(int i = start + 1; i < data.Length; i++)
+        {
+            if(data[i] < data[min])
+            {
+                min = i;
+            }
+        }
+        return min;
+    }
+    
+    static void IntArraySelectionSort(int[] data)
+    {
+        for(int i = 0; i < data.Length - 1; i++)
+        {
+            int min = IntMinArray(data, i);
+            if(i != min)
+            {
+                Swap(data, i, min);
+                string numbers = String.Join(",", data);
+                Console.WriteLine(numbers);
+                
+            }
+        }
+    }
+    
+    static void IntArrayInsertionSort(int[] data)
+    {
+        for(int j = 1; j < data.Length; j++)
+        {
+            for(int i = j; i > 0; i--)
+            {
+                if(data[i] < data[i-1])
+                {
+                    Swap(data, i, i-1);
+                    string numbers = String.Join(",", data);
+                    Console.WriteLine(numbers);
+                }
+            }
+        }
+    }
+    
+    static void IntArrayQuickSortRecursive(int[] data, int left, int right)
+    {
+        int i = left;
+        int j = right;
+        int pivot = data[(left + right) / 2];
+        Console.WriteLine(pivot);
+        while (true)
+        {
+            while(data[i] < pivot)
+            {
+                Console.WriteLine($"i {i} value {data[i]} is less than pivot {pivot}");
+                i++;
+            }
+            while(pivot < data[j])
+            {
+                Console.WriteLine($"pivot {pivot} is less than j {j} value {data[j]}");
+                j--;
+            }
+            if(i <= j)
+            {
+                Console.WriteLine($"i {i} value {data[i]} is <= j {j} value {data[j]}");
+                Swap(data, i, j);
+                string numbers = String.Join(",", data);
+                Console.WriteLine(numbers);
+                i++;
+                j--;
+            }
+            if(i > j)
+            {
+                Console.WriteLine("break");
+                break;
+            }
+        }
+        if(left < j)
+        {
+            Console.WriteLine($"Calling recursive left {left} and j {j}");
+            IntArrayQuickSortRecursive(data, left, j);
+        }
+        if(i < right)
+        {
+            Console.WriteLine($"Calling recursive right {right} and i {i}");
+            IntArrayQuickSortRecursive(data, i, right);
+        }
+    }
+    
+    static void IntArrayQuickSort(int [] data)
+    {
+        IntArrayQuickSortRecursive(data, 0, data.Length - 1);
+    }
+    
+    static void Main()
+    {
+        int[] myArray = {6,4,1,8,5,7,3,0,9,2};
+        
+        string numbers = String.Join(",", myArray);
+        Console.WriteLine(numbers);
+
+        //Swap(myArray, 2, 3);
+        //BubbleSort(myArray);
+        //int minInArray = IntMinArray(myArray, 3);
+        //IntArraySelectionSort(myArray);
+        //IntArrayInsertionSort(myArray);
+        IntArrayQuickSort(myArray);
+        
+        numbers = String.Join(",", myArray);
+        Console.WriteLine(numbers);
+
+    }
+}
